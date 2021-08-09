44b3026f0deff39eb9992e9b08e1d38d9fc7379f
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-08-09 13:26:59 +0000

    no query admitted

--- APIs/votes/voteapi.js
+++ APIs/votes/voteapi.js
@@ -44,7 +44,7 @@ module.exports ={
 			console.log(req.data)
 			//console.log(req)
 
-			const output = req.query ? req.query : JSON.parse(req.body);
+			const output = req.body;
 			const userid = output.user;
 			if(!userid) return res.status(400).json({ body:req.body, err:true, code:403, message:'Invalid userid !' });
 			console.log('Vote received on /topgg');
